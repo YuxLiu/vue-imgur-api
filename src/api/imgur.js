4fc08fdd9b7c416e83a6b4cea8681255df8521ac
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-12 22:04:31 +0800

    fetch img axios

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,10 +1,10 @@
 import qs from 'qs';
-
+import axios from 'axios'
 const CLIENT_ID = '678985071d92f0a';
 const ROOT_URL = 'https://api.imgur.com';
 
 export default {
-  login:() => {
+  login() {
     const querystring = {
       client_id: CLIENT_ID,
       response_type: 'token',
@@ -13,4 +13,12 @@ export default {
     window.location =
       `${ROOT_URL}/oauth2/authorize?${qs.stringify(querystring)}`;
   },
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+  }
 }
