34881a099b0ade88649969e069a0f4f599a69d92
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-13 00:32:38 +0800

    finished.

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <img v-for="image in allImages" :src="image.link"/>
+    <div v-if="isLoggedIn" class="image-container">
+      <img v-for="image in allImages" :src="image.link"/>
+    </div>
+    <div v-else>Logged in to continue</div>
   </div>
+
 </template>
 
 <script>
@@ -9,7 +13,7 @@ import {mapActions, mapGetters} from 'vuex';
 
 export default {
   name: "ImageList",
-  computed: mapGetters(['allImages']),
+  computed: mapGetters(['allImages', 'isLoggedIn']),
   methods: {
     ...mapActions(['fetchImages']),
   },
@@ -20,5 +24,13 @@ export default {
 </script>
 
 <style scoped>
+.image-container {
+  column-count: 3;
+  column-gap: 0;
+}
 
+img {
+  max-width: 100%;
+  padding: 5px;
+}
 </style>
