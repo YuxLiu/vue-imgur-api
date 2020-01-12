af35b167bc67c49de1e6fb45cf53ebc7ce0b4888
Ethan Liu <liuyuxuan1230@outlook.com>
2020-01-12 23:21:21 +0800

    116 : rootState and async&await

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -12,6 +12,7 @@ const actions = {
   async fetchImages({ rootState }) {
     const { token } = rootState.auth;
     const response = await api.fetchImages(token);
+    console.log(response);
   }
 };
 
@@ -19,4 +20,11 @@ const mutations = {
   setImages: (state, images) => {
     state.images = images;
   }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
 };
